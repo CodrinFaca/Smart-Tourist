0b29a4f5fdcde1db184d879cce04abd1da1c1eee
ionutfaca <faca.ionut@yahoo.com>
2015-11-14 17:49:07 +0200

    Localization

---
+++ SmartTourist/SmartTourist.Domain/Localization/LocalizationKeys.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTourist.Domain.Localization
+{
+    //Clasa asta e folosita ca sa nu scrii de mana stringuri -> nu gresesti.
+    //Daca vrei sa folosestii ca si key Login, apelezi ca in metoda GetValue
+    
+    public class LocalizationKeys
+    {
+        public static string Login = "Login";
+
+        public string GetValue(string test)
+        {
+            test = LocalizationKeys.Login;
+            List<LocalizationStrings> local = new List<LocalizationStrings>();
+            string stringLocalizat = local.Where(x => x.Key.Equals(test) && x.LanguageCode.Equals("en")).FirstOrDefault().Value;
+            return stringLocalizat;
+            
+        }
+    }
+}
